a38ea7ce2d113f091dee627d3b1638b0026803b3
Nataliya Perevoshchikova <47018678+NataCalc@users.noreply.github.com>
2022-04-07 21:51:38 +1000

    Add files via upload

--- mcode/C_BodyParts3D_isolate_LegOuterSkinSurface_LegMuscles_vs1.m
+++ mcode/C_BodyParts3D_isolate_LegOuterSkinSurface_LegMuscles_vs1.m
@@ -12,7 +12,7 @@ clear; close all; clc;
 
 %% Plotting settings
 lineWidth=0.5;
-
+fontSize=40;
 %% Control parameters
 
 % Path names
@@ -40,14 +40,22 @@ switch Select_layer
         V=model.vertices;
 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %% Working with original file of the skin to cut the right leg preserving
-        %everithing below the sex glands.
+        %% Working with original file of the body surfaces wrapped around organs to cut the right side of the body preserving
+        %% everithing below the cutHeight
         logicVertices=V(:,1)<0 & V(:,3)<=cutHeight+mean(patchEdgeLengths(F,V));
         logicFaces=any(logicVertices(F),2);
         logicFaces=triSurfLogicSharpFix(F,logicFaces);
         Fs=F(logicFaces,:);
         [Fs,Vs]=patchCleanUnused(Fs,V);
         
+        cFigure; hold on;        
+        gpatch(Fs,Vs,'w','none',0.25);
+        axis off;
+        axisGeom;
+        camlight headlight;
+        drawnow;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Cut again preserving only right leg
         optionStruct.outputType='label';
         [G,~,groupSize]=tesgroup(Fs,optionStruct);
         [~,indMax]=max(groupSize);
@@ -55,24 +63,61 @@ switch Select_layer
         Fs=Fs(logicKeep,:);
         [Fs,Vs]=patchCleanUnused(Fs,Vs);
         
+        cFigure; hold on;
+        gpatch(Fs,Vs,'w','none',0.25);
+        axis off;
+        axisGeom;
+        camlight headlight;
+        drawnow;
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Slice the surface of the right leg at cutHeight
+        %% to detect all edges to form the list of curves within the slice
         snapTolerance=mean(patchEdgeLengths(Fs,Vs))/100;
         n=vecnormalize([0 0 1]); %Normal direction to plane
         P=[0 0 cutHeight]; %Point on plane
         [Fc,Vc,~,logicSide,Ec]=triSurfSlice(Fs,Vs,[],P,n,snapTolerance);
         groupStruct.outputType='label';
         [G,~,groupSize]=tesgroup(Ec,groupStruct);
-        [~,indKeep]=max(groupSize);
+        
+        for i=1:size(groupSize,2)
+            logicKeep=G==i;
+            Ec_keep=Ec(logicKeep,:);
+            indCurveNow=edgeListToCurve(Ec_keep);
+            VTcc{i}=Vc(indCurveNow,:);
+              
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Find in the list the curve with the min value along X axis with asociated index 
+        %% which corresponds to the edge of the skin surface
+        for i=1:size(groupSize,2)
+            [val(i),idx(i)] = min([VTcc{i}(:,1)]);   
+        end
+        [~,indKeep]=min(val(:));
         logicKeep=G==indKeep;
         Ec_keep=Ec(logicKeep,:);
         indCurveNow=edgeListToCurve(Ec_keep);
         VTc=Vc(indCurveNow,:);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Forming the 2D convex hull around the curve
         [V_slice_curve]=convexhull_curve(VTc,200);
-        
         [Fc,Vc]=patchCleanUnused(Fc(logicSide,:),Vc);
+        
+        cFigure; hold on;        
+        gpatch(Fc,Vc,'w','none',0.25);
+        plotV(V_slice_curve,'k-','LineWidth',4);
+        axis off;
+        axisGeom;
+        camlight headlight;
+        drawnow;
+
+        %% Working with right leg to cut preserving
+        %% everyting below the sex glands.
         %Set-up orientation and location of cutting plane
         snapTolerance=mean(patchEdgeLengths(Fc,Vc))/100; %Tolerance for surface slicing
         n=vecnormalize([0 0 1]); %Normal direction to plane
         Q1=euler2DCM([0.4*pi 0 0.8*pi]);
+
         n=n*Q1;
         P_cut=[0 0 0]+n*306; %Point on plane
         %Slicing surface
@@ -81,9 +126,7 @@ switch Select_layer
         [Fe,Ve]=patchCleanUnused(Fd(logicSide==1,:),Vd);
 
             
-        cFigure; hold on;
-        title('The femur surface');
-        
+        cFigure; hold on;        
         gpatch(F,V,'w','none',0.25);
         gpatch(Fe,Ve,'rw','none',1);
         plotV(V_slice_curve,'k-','LineWidth',4);
@@ -92,8 +135,9 @@ switch Select_layer
         camlight headlight;
         drawnow;
         
-        Ve=[Ve;V_slice_curve;];
-
+        %% Forming the point cloud out of the processed cut right leg a
+        %% and the edge of the skin (curve) formed at cutHeight
+        Ve=[Ve;V_slice_curve;];    
         %% Construct alpha shape
         % This deteriorates surface quality (bridges concave regions) but is a
         % termporary "quick-and-dirty" work-around to obtain outer skin surface
@@ -107,20 +151,26 @@ switch Select_layer
         optionStructRemesh.disp_on=1; % Turn off command window text display
         [Fn,Vn]=ggremesh(Fa,Va,optionStructRemesh);
         
-        %% Visualisation
-        
+        %% Visualisation       
         cFigure;
-        subplot(1,2,1); hold on;
-        gpatch(F,V,'w','none',0.25);
-        gpatch(Fn,Vn,'bw','none');
+        hAxis(1)=subplot(1,2,1); hold on;
+        %gpatch(Fs,Vs,'w','none',0.25);
+        gpatch(Fe,Ve,'w','none',1);
+        plotV(V_slice_curve,'k-','LineWidth',4);
+
+        text(min(V(:,1))+50,min(V(:,2))+99,'A','FontSize',fontSize);
         axis off;
         axisGeom;
         camlight headlight;
         
-        subplot(1,2,2); hold on;
-        gpatch(Fn,Vn,'bw','k',1,lineWidth);
+        hAxis(2)=subplot(1,2,2); hold on;
+        gpatch(Fn,Vn,'w','k',1,lineWidth);
+        text(min(V(:,1))+40,min(V(:,2))+110,'B','FontSize',fontSize);
         axis off;
         axisGeom;
+        pos = get( hAxis(2), 'Position' );
+        pos(1)=pos(1)-0.3;
+        set( hAxis(2), 'Position', pos ) ;
         camlight headlight;
         gdrawnow;
         
@@ -144,46 +194,29 @@ switch Select_layer
         Fr=model.faces;
         Vr=model.vertices;
 
-
-        %% Define the muscle layer. This involves working with the remesh mesh of the
-        %original skin mesh. Right now it's the manual process.
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %Cut the right leg preserving everithing below the sex glands.
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Define the muscle.         
+        %% Working with original file of the body surfaces wrapped around organs to cut the right side of the body preserving
+        %% everything below the cutHeight
         logicVertices=Vr(:,1)<1 & Vr(:,3)<=cutHeight+mean(patchEdgeLengths(Fr,Vr));
         logicFaces=any(logicVertices(Fr),2);
         logicFaces=triSurfLogicSharpFix(Fr,logicFaces);
         Fs=Fr(logicFaces,:);
         [Fs,Vs]=patchCleanUnused(Fs,Vr);
         
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Cut preserving everything from the right leg
         optionStruct.outputType='label';
         [G,~,groupSize]=tesgroup(Fs,optionStruct);
         [~,indMax]=max(groupSize);
         logicKeep=G==indMax;
         Fs=Fs(logicKeep,:);
-        [Fs,Vs]=patchCleanUnused(Fs,Vs);
+        [Fe,Ve]=patchCleanUnused(Fs,Vs);
         
-        snapTolerance=mean(patchEdgeLengths(Fs,Vs))/100;
-        n=vecnormalize([0 0 1]); %Normal direction to plane
-        P=[0 0 cutHeight]; %Point on plane
-        [Fc,Vc,~,logicSide]=triSurfSlice(Fs,Vs,[],P,n,snapTolerance);
-        [Fc,Vc]=patchCleanUnused(Fc(logicSide,:),Vc);
-        
-        %Set-up orientation and location of cutting plane
-        snapTolerance=mean(patchEdgeLengths(Fc,Vc))/100; %Tolerance for surface slicing
-        n=vecnormalize([0 0 1]); %Normal direction to plane
-        P_cut=[0 0 0]+n*cutHeight; %Point on plane
-        
-        %Slicing surface
-        [Fd,Vd,Cd,logicSide,~]=triSurfSlice(Fc,Vc,[],P_cut,n);
-        %Compose isolated cut geometry and boundary curves
-        [Fe,Ve]=patchCleanUnused(Fd(logicSide==1,:),Vd);
-        
-        cFigure; hold on;
-        title('The femur surface');
-        
+        cFigure; hold on;  
         gpatch(Fr,Vr,'w','none',0.25);
-        gpatch(Fe,Ve,'rw','none',1);
-        
+        gpatch(Fe,Ve,'r','none',1);
+        axis off;
         axisGeom;
         camlight headlight;
         drawnow;
@@ -192,21 +225,23 @@ switch Select_layer
         numLoftGuideSlicesFat=60;
         n=vecnormalize([0 0 1]);
         numPoints=200;
-        %Find lowest and highest point on the skin
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Set up lower and upper limits for the muscle height
+        %% Find these limits on the skin surface
         [~,indMax]=max(Ve(:,3));
         [~,indMin]=min(Ve(:,3));
         P=Ve(indMax,:);
         PA=Ve(indMin,:);
-        P(:,3)=P(:,3)-113;
+        P(:,3)=P(:,3)-122.5;%Upper limit 1
         P0=P;
-        PA(:,3)=PA(:,3)+110;
-        zz=linspacen(P(:,3),PA(:,3),numLoftGuideSlicesFat);
+        PA(:,3)=PA(:,3)+110;%Lower limit
+        zz=linspacen(P(:,3),PA(:,3),numLoftGuideSlicesFat);%number of slices within the range between limits
         e=[(1:numPoints)' [(2:numPoints)';1]];
         VC=cell(numLoftGuideSlicesFat,1);
         
         q=1;
         P=Ve(indMax,:);
-        P(:,3)=P(:,3)-6;
+        P(:,3)=P(:,3)-10;%Upper limit
         [Ff,Vf,~,logicSide,Eb]=triSurfSlice(Fe,Ve,[],P,n,snapTolerance);
         groupStruct.outputType='label';
         [G,~,groupSize]=tesgroup(Eb,groupStruct); 
@@ -218,26 +253,36 @@ switch Select_layer
             [V_slice_curve]=convexhull_curve(VT,numPoints);
             Vcc{i}=V_slice_curve;
         end
-%         
-%         %first_min_value
-%         for i=1:size(groupSize,2)
-%             [val(i),idx(i)] = min([Vcc{i}(:,1)]);   
-%         end
-%         [~,idx_a]=min(val(:));
-%         Vcc{idx_a} = NaN;
-%         %second_min_value
-%         for i=1:size(Vcc,2)
-%             [val(i),idx(i)] = min([Vcc{i}(:,1)]);   
-%         end
-%         [~,idx_a]=min(val(:));
-%         Vcc{idx_a} = NaN;
-%         %third_min_value
-%         for i=1:size(Vcc,2)
-%             [val(i),idx(i)] = min([Vcc{i}(:,1)]);   
-%         end
-%         [~,idx_a]=min(val(:));
-        idx_a=3;
+
+        cFigure;hold on;
+        gpatch(Ff(logicSide,:),Vf,'w','none',1);
+        gpatch(Ff(~logicSide,:),Vf,'w','none',0.25);
+        gpatch(Eb,Vf,'none','b',1,3); 
+        for q=1:1:size(Vcc,2)
+            Vpp=Vcc{q};
+            plotV(Vpp([1:size(Vpp,1) 1],:),'k-','LineWidth',4);
+        end
+        axisGeom; axis off;
+        axis manual; camlight headligth;
+        set(gca,'FontSize',25);
+        gdrawnow;
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% first_min_value along X axis which corresponds to the edge of the skin surface
+        for i=1:size(groupSize,2)
+            [val(i),idx(i)] = min([Vcc{i}(:,1)]);   
+        end
+        [~,idx_a]=min(val(:));
+        index=linspace(1,size(groupSize,2),size(groupSize,2));
+        index1 = index(find(index~=idx_a));
+        %% second_min_value along X axis which corresponds to the edge of the muscle surface
+        for i=1:size(index1,2)
+            [val(i),idx(i)] = min([Vcc{index1(i)}(:,1)]);   
+        end
+        [~,idx_a]=min(val(:));
         V_slice_curve=Vcc{idx_a};
+        
+        %% Reference upper curve corresponds to the beginning of the muscle layer
         VC{q}=V_slice_curve;
         FL=[];
         VL=V_slice_curve;
@@ -245,7 +290,13 @@ switch Select_layer
         if indStart>1
             VL=[VL(indStart:end,:); VL(1:indStart-1,:)];
         end
-
+        
+        VL0=VL;
+        VL(:,1)=0.7.*VL(:,1);
+        Xshift=mean(VL,1)-mean(VL0,1);
+        VL(:,1)=VL(:,1)-Xshift(1)-30; 
+        
+        VL0=VL;
         [Fh,Vh]=regionTriMesh3D({VL},[],0,'linear');
         Fh=fliplr(Fh);
         
@@ -253,29 +304,32 @@ switch Select_layer
         cFigure;hold on;
         gpatch(Ff(logicSide,:),Vf,'w','none',1);
         gpatch(Ff(~logicSide,:),Vf,'w','none',0.25);
-        %gpatch(Fh,Vh,'w','k',0.25);
+        gpatch(Fh,Vh,'w','k',0.25);
         gpatch(Eb,Vf,'none','b',1,3);
         plotV(VL,'k-','LineWidth',4);
-        axisGeom; axis off;axis manual; camlight headligth;
+        axisGeom; %axis off;
+        axis manual; camlight headligth;
         set(gca,'FontSize',25);
         gdrawnow;
 
+        VC{1}=VL;
+        %% Loop over number of slices along Z axis
         for i=2:1:numLoftGuideSlicesFat
             P(:,3)=zz(i);
             [Ff,Vf,~,logicSide,Eb]=triSurfSlice(Fe,Ve,[],P,n,snapTolerance);
             groupStruct.outputType='label';
             [G,~,groupSize]=tesgroup(Eb,groupStruct);
-
             
-            for q=1:1:max(size(groupSize))
-            logicKeep=G==q;
-            Eb_keep=Eb(logicKeep,:);
-            indCurveNow=edgeListToCurve(Eb_keep);
-            VT=Vf(indCurveNow,:);
-            [V_slice_curve]=convexhull_curve(VT,numPoints);
-            Vcc{i}=V_slice_curve;
-            end
-
+            %
+            %             for q=1:1:max(size(groupSize))
+            %             logicKeep=G==q;
+            %             Eb_keep=Eb(logicKeep,:);
+            %             indCurveNow=edgeListToCurve(Eb_keep);
+            %             VT=Vf(indCurveNow,:);
+            %             [V_slice_curve]=convexhull_curve(VT,numPoints);
+            %             Vcc{i}=V_slice_curve;
+            %             end
+            
             VT={};
             for q=1:1:max(size(groupSize))
                 logicKeep=G==q;
@@ -284,7 +338,7 @@ switch Select_layer
                 VT{q}=Vf(indCurveNow',:);
                 Xmin(q)=min(VT{q}(:,1));
             end
-
+            
             [Xmin_value,indOut]=min(Xmin);
             count=1;
             VTT={};
@@ -298,13 +352,13 @@ switch Select_layer
                     count=count+1;
                 end
             end
-        
+            
             V_points_keep=[];
             for q=1:1:max(size(VTT))
                 Vpp=VTT{q};
                 V_points_keep=[V_points_keep;Vpp];
             end
-
+            
             [V_slice_curve]=convexhull_curve(V_points_keep,numPoints);
             f=[e+(i-2)*numPoints fliplr(e)+(i-1)*numPoints];
             
@@ -321,51 +375,36 @@ switch Select_layer
 %             axisGeom; axis off;axis manual; camlight headligth;
 %             set(gca,'FontSize',25);
 %             gdrawnow;
-        end
 
+        end
+  
         [Fg,Vg]=regionTriMesh3D({V_slice_curve},[],0,'linear');
         ne=mean(patchNormal(Fg,Vg),1);
         if dot(ne,[0 0 1])>0
             Fg=fliplr(Fg);
         end
        
-        %VL0=VL;
         [FL,VL]=subQuad(FL,VL,3,3);
-        %[FL,VL]=subQuadCatmullClark(FL,VL,2,1);
         [FL,VL]=quad2tri(FL,VL,'a');
         FL=fliplr(FL);
         
-        cFigure;
+        cFigure;        
         subplot(1,2,1); hold on;
-        %gpatch(Fe,Ve,'rw','none',1);
         gpatch(Ff(logicSide,:),Vf,'w','none',1);
         gpatch(Ff(~logicSide,:),Vf,'w','none',0.25);
-        %gpatch(Eb,Vf,'none','b',1,3);
-        plotV(VL,'k-','LineWidth',4);
+        for q=1:1:size(VC,1)
+            Vpp=VC{q};
+           
+            plotV(Vpp([1:size(Vpp,1) 1],:),'k-','LineWidth',4);
+        end
         axisGeom;axis off;
         camlight headligth;
         set(gca,'FontSize',25);
         
         subplot(1,2,2); hold on;
-        %plotV(P0,'g.','MarkerSize',30);
-        %plotV(PA,'g.','MarkerSize',30);
-        gpatch(Fg,Vg,'w','k',1);
-        %patchNormPlot(Fg,Vg);
-        
-        gpatch(Fh,Vh,'w','k',1);
-        %patchNormPlot(Fh,Vh);
-        
-        gpatch(FL,VL,'bw','k',1);
-        %patchNormPlot(FL,VL);
-        gpatch(Ff,Vf,'w','none',0.25);
-        %plotV(VL0,'k-','LineWidth',4);
-        nn=size(VC);
-        for q=1:1:nn(1)
-            Vpp=VC{q};
-            plotV(Vpp([1:size(Vpp,1) 1],:),'k-','LineWidth',4);
-        end
-        
-        axisGeom; axis off;axis manual; camlight headligth;
+        gpatch(FL,VL,'bw','k',1);        
+        axisGeom; axis off;
+        camlight headligth;
         set(gca,'FontSize',25);
         gdrawnow;
 
@@ -378,7 +417,7 @@ switch Select_layer
         gpatch(Ff,Vf,'w','none',0.5);
         gpatch(F_muscles,V_muscles,C_muscles,'none',1);
 
-        for q=1:1:nn(1)
+        for q=1:1:size(VC,1)
             Vpp=VC{q};
             plotV(Vpp([1:size(Vpp,1) 1],:),'k-','LineWidth',4);
         end
@@ -397,12 +436,41 @@ switch Select_layer
         [Fn,Vn]=ggremesh(F_muscles,V_muscles_smoothed,optionStructRemesh);
     
         cFigure;hold on;
-        gpatch(Fe,Ve,'w','none',1);
+        gpatch(Fe,Ve,'w','none',0.1);
         gpatch(Fn,Vn,'r','k',1);
         axisGeom; axis off; axis manual; camlight headligth;
         set(gca,'FontSize',25);
         gdrawnow;
         
+        cFigure;        
+        a1=subplot(1,2,1); hold on;
+        gpatch(Ff(logicSide,:),Vf,'w','none',1);
+        gpatch(Ff(~logicSide,:),Vf,'w','none',0.25);
+        for q=1:1:size(VC,1)
+            Vpp=VC{q};
+            plotV(Vpp([1:size(Vpp,1) 1],:),'k-','LineWidth',4);
+        end
+        
+        text(min(Vf(:,1))-100,min(Vf(:,2))+130,'A','FontSize',fontSize);
+        axisGeom;axis off;
+        axis manual; 
+        camlight headligth;
+        set(gca,'FontSize',25);
+
+        a2=subplot(1,2,2); hold on;
+        %gpatch(Fe,Ve,'w','none',0.1);
+        gpatch(Fn,Vn,'w','k',1);
+        text(min(Vn(:,1))+50,min(Vn(:,2))+250,'B','FontSize',fontSize);
+        axisGeom; axis off; 
+        axis manual; 
+        pos = get( a2, 'Position' );
+        pos(1)=pos(1)-0.3;
+        set( a2, 'Position', pos ) ;
+        camlight headligth;
+        set(gca,'FontSize',25);   
+        gdrawnow;
+        
+
         %% Store model in structure
         modelNew.source=fileName_FMA_refined;
         modelNew.faces=Fn;
